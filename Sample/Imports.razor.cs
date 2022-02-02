00f10dbef234f1f9187ae36b33a4bdf13748d25c
Jay Krishna Reddy <jaykrishnareddy@gmail.com>
2022-02-02 17:00:28 +0530

    Add project files.

---
+++ Sample/Imports.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Sample
+{
+    public partial class Imports
+    {
+        public Imports()
+        {
+
+        }
+    }
+}
